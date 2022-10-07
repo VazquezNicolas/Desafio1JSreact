9b381ebd6cf306813d555870284473f15c9ef35b
VazquezNicolas <nicolasorlandovazquez@gmail.com>
2022-10-06 22:16:03 -0300

    Agregue cartWidget y ItemListContainer

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Navbar from './components/navbar/navbarJSX';
+import ItemListContainer from './components/itemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <ItemListContainer greeting = {'¡Bienvenido a Los Herederos de Baden Powell!'}/>
     </div>
   );
 }
--- app/src/components/navbar/navbarJSX.js
+++ app/src/components/navbar/navbarJSX.js
@@ -1,17 +1,19 @@
 import './navbarJSX.css'
 import favicon from './assets/img/favicon3.jpg'
+import CartWidget from '../CartWidget/CartWidget'
 
 const Navbar = ()=> {
     return (
-        <nav className="navBar">
+        <nav className="navBar ">
             <div className=" container centrar ">
                 <img className="favIcon col-1" src={favicon}/>
-                <div className="col-11">
+                <div className="col-10">
                 <h1>Los Herederos de Baden Powell</h1>
                     <button>Pasapañuelos</button>
                     <button>Mates</button>
                 </div>
             </div>
+            <CartWidget/>
         </nav>
     )
 }
